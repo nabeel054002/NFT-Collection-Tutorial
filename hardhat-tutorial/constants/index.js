44273430edc77369b2450a714e13d3a69d19dafb
Nabeel Khan <nkhan054002@gmail.com>
2022-09-04 17:51:02 +0530

    first commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x742B19Ace9B406b53F5c568ff09dded18a019776";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-ten-delta.vercel.app/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+//0xA4f46D6b3Af2BcAd56cd9B8014870c2f8522C84a
